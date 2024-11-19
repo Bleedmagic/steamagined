335c4927da709461a8d2f70abecd44837422ea93
Bleedmagic <Bleedmagic87@gmail.com>
2024-11-19 09:14:24 +0800

    Tue, Nov 19, 2024  9:14:13 AM

--- js/type-animation.js
+++ js/type-animation.js
@@ -1,24 +1,38 @@
 var typed1 = new Typed("#text-animation-typing-1", {
 	strings: [
-		"Welcome to <i>Steamagined</i>.<br> A portfolio focused on my Steam gaming!<br>Feel free to explore~",
+		"Welcome to <i>Steamagined</i>.<br> A portfolio focused on my Steam gaming!<br>Feel free to explore~ :D",
 	],
 	loop: false,
 	typeSpeed: 60,
-	backSpeed: 25,
-	backDelay: 1500,
 	showCursor: true,
 	cursorChar: "_",
 	contentType: "html",
 	startDelay: 2750,
 });
 
-var typed2 = new Typed("#text-animation-typing-2", {
-	strings: [""],
-	loop: false,
-	typeSpeed: 60,
-	backSpeed: 25,
-	backDelay: 1500,
-	showCursor: true,
-	cursorChar: "_",
-	contentType: "html",
-});
+function startTyped2() {
+	var typed2 = new Typed("#text-animation-typing-2", {
+		strings: [
+			"Thank you for visiting <i>Steamagined</i>!<br>I hope you enjoyed your stay.<br>Have a nice day~ :)",
+		],
+		loop: false,
+		typeSpeed: 60,
+		showCursor: true,
+		cursorChar: "_",
+		contentType: "html"
+	});
+}
+
+const observer = new IntersectionObserver(
+	(entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				startTyped2();
+				observer.disconnect();
+			}
+		});
+	},
+	{ threshold: 0.5 }
+);
+
+observer.observe(document.getElementById("text-animation-typing-2"));
